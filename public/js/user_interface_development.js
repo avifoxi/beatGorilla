10bbee906c41159a2686bf7abfb8171786bd4a7f
Alan Cohen & garrett dalton <pair+acjetnut.garrettdalton@devbootcamp.com>
2014-08-15 17:21:28 -0400

    implemented basic logic to iterate through soundpatterns

--- public/js/user_interface_development.js
+++ public/js/user_interface_development.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+$(document).ready(function() {
+
+    var num_soundpatterns = $('tr').length;
+    var num_beats_per_soundpattern = $('td').length / num_soundpatterns;
+    var id=1;
+
+
+    function highlightColumn() {
+        $('td').css("background", "white");
+        $('td:nth-child(' + id + ')').css("background", "yellow");
+        id += 1;
+        if (id > 8) { id = 1; };
+    }
+
+
+    $('#play_button').on('click', highlightColumn);
+
 });
+
+
+
+
+
+
+    // function loopSequence() {
+    //     for (i=1; i<=num_beats_per_soundpattern; i++) {
+    //         $('#play_button').on('click', highlightColumn(i));
+    //         // $('td').css("background", "white");
+    //     };
+    // };
