783c5bf55bfa0e610c0f47a0d217d91ed631b8cb
Edward Kim <eddiekimosabe@gmail.com>
2014-08-15 18:29:55 -0400

    created login/signup/userprofile

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,12 @@
-get 'users/:id' do
-  # user profile page
+post '/users/new' do
+  @user = User.new(params[:signup])
+  @user.password = params[:signup][:password]
+  @user.save
+  session[:user_id] = @user.id
+  redirect '/'
+end
+
+get '/users/:id' do
+  @user = User.find(session[:user_id])
+  erb :user_profile
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
-  has_many :sequences
+has_many :sequences
+  include BCrypt
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
