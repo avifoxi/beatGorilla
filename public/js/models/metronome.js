38aace898ecfe31cb0d00c86a579c09711da738d
Avi Fox-Rosen & John Berry <pair+avifoxi.jpberry0623@devbootcamp.com>
2014-08-15 17:50:33 -0400

    mvp metronome, attempting set timeout for timing

--- public/js/models/metronome.js
+++ public/js/models/metronome.js
@@ -1,10 +1,11 @@
 console.log('hello from Metronome!')
 
-
+// placeholder - this is a dumb metronome - but it works for MVP
 
 function Metronome(context, tempoBpm) {
     this._context = context;
-    this._tempoBpm = tempoBpm;
+    this._tempoBpm = 500;
+    this._isPlaying = false;
 
     this.beatDropped = new Event(this);
 
@@ -14,7 +15,27 @@ function Metronome(context, tempoBpm) {
 }
 
 Metronome.prototype = {
-    proxyBeat : function() {
+    dropBeat : function() {
         this.beatDropped.notify();
+    },
+    startPlay : function () {
+        var _this = this;
+        this._isPlaying = true;
+        this.recursiveTimer()
+    },
+    recursiveTimer : function() {
+
+        var _this = this;
+        var internalTempo = this._tempoBpm;
+        console.log(internalTempo);
+        // window.setTimeout( function(){
+        //     _this.dropBeat();
+        // }, internalTempo);
+        // if (this._isPlaying === true) {
+        //     _this.startPlay(this._tempoBpm);
+        // }
     }
 }
+
+
+
