d979ccf39db2d7bfb04b3737b2b878bd5bb5bfe1
Avi Fox-Rosen <frosenox@gmail.com>
2014-08-16 19:38:01 -0400

    adding true paths in Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@ seq_one = Sequence.create(name: "sequence1", tempo: 120, steps: 8, user_id: eddi
 
 # TO DO: update the sounds in seed with the routes to actual sounds in public/sounds/mvp_sounds folder, ala: kick => '/sounds/mvp_sounds/Kick_ac_close.wav'
 
-sounds = %w(snare kick cymbal tom)
-sounds.each {|sound| sound = Sound.create(name: "#{sound}", url: "www.#{sound}.com")}
+Sound.create(name: 'mvpSnare', url: '/sounds/mvp_sounds/Snare_ac_close.wav')
+Sound.create(name: 'mvpKick', url: '/sounds/mvp_sounds/Kick_ac_close.wav')
+Sound.create(name: 'mvpCrash1', url: '/sounds/mvp_sounds/Crash1_ac_close.wav')
+Sound.create(name: 'mvpCrash2', url: '/sounds/mvp_sounds/Crash2_ac_close.wav')
+Sound.create(name: 'mvpHihat', url: '/sounds/mvp_sounds/ClosedHat_ac_close.wav')
+Sound.create(name: 'mvpLowTom', url: '/sounds/mvp_sounds/LowTom_ac_close.wav')
+Sound.create(name: 'mvpMidTom', url: '/sounds/mvp_sounds/MidTom_ac_close.wav')
+Sound.create(name: 'mvpTambr', url: '/sounds/mvp_sounds/Tamb_ac_close.wav')
+
 
 
 
 
 soundPs = %w(sp1 sp2 sp3 sp4)
 soundPs.each_with_index {|sp, i| sp = SoundPattern.create(sound_id: (i + 1), sequence_id: seq_one.id)}
+
 
 SoundPattern.all.each do |sp|
   i = 1
