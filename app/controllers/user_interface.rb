04547c294f06298b59d4f9c3c42aca33f1495c0b
Alan Cohen <awc583@gmail.com>
2014-08-16 15:48:35 -0400

    fixed duplicate controller files

--- app/controllers/user_interface.rb
+++ app/controllers/user_interface.rb
@@ -15,7 +15,6 @@ end
 
 
 
-# BUGGY IMPLEMENTATION, NEEDS TO BE FIXED!!!!!
 put '/ui/:id/save' do
   active_beats = []
 
@@ -40,4 +39,25 @@ put '/ui/:id/save' do
   end
 
   redirect "/ui/#{params[:id]}"
+end
+
+
+
+
+post '/ui/create' do
+
+  seq = Sequence.create(name: params["seq_name"], tempo: params["seq_tempo"], steps: params["seq_beats"], user_id: params["seq_user_id"])
+
+  all_patterns = params.select { |k, v| k.include? "pattern" }
+
+  all_patterns.each_with_index do |(pattern, beats), i|
+    sound_p = SoundPattern.create(sound_id: params["sound#{i+1}_id"], sequence_id: seq.id)
+    seq.steps.times do |index|
+      beats.has_key?((index + 1).to_s) ? play_beat = true : play_beat = false
+      Beat.create(play: play_beat, position: (index + 1), sound_pattern_id: sound_p.id)
+    end
+  end
+
+  @user = User.find(seq.user_id)
+  redirect "/users/#{@user.id}"
 end
