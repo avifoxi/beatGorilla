b63cf466c2336b2160b3a008919e122b87f46310
Avi Fox-Rosen <frosenox@gmail.com>
2014-08-17 17:32:50 -0400

    slowly generating dynamic components of sequence table, in dynmaic.js

--- public/js/dynamic.js
+++ public/js/dynamic.js
@@ -1,4 +1,11 @@
 console.log('hello from dynamic.js')
 
+var title = document.getElementById('sequenceName')
+var seq = controller._model._sequence
 
+var sequenceName = document.createElement('h2')
+sequenceName.innerHTML = seq.name
+title.appendChild(sequenceName)
 
+var table = document.getElementsByTagName('table')[0]
+var row = document.createElement('tr')
--- public/js/views/view.js
+++ public/js/views/view.js
@@ -29,7 +29,7 @@ function View(model, elements) {
 
 View.prototype = {
   newBeat : function() {
-    console.log('wake up view, its beat time!');
+    // console.log('wake up view, its beat time!');
     this.highlightCurrentBeat();
     this.checkForScheduled();
   },
