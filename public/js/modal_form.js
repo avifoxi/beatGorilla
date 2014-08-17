eeef11ff2677cbe69b6ccc37e8a6c79208f993f5
Edward Kim <eddiekimosabe@gmail.com>
2014-08-16 21:09:23 -0400

    added modal login/signup functionality. Need to add ajax.

---
+++ public/js/modal_form.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  console.log("hi")
+
+  $("#loginButton").click(function(event){
+    event.preventDefault();
+    $("form#loginForm").fadeIn( "slow", function(){
+    });
+  });
+
+  $("#loginAway").click(function(event){
+    event.preventDefault();
+    $("form#loginForm").fadeOut( "slow", function(){
+      });
+  });
+
+  $("form#loginForm").submit(function(event){
+    event.preventDefault();
+
+  // $.ajax({
+  //   url: "/login",
+  //   type: "POST"
+  //   data: $(this).serialize(),
+  //   dataType: "html",
+  //   success: function(data) {
+  //     console.log(data)
+
+  //     // $('.navigation').html(data);
+  //     //   // $('#nav').remove();
+  //     //   // $();
+  //     // $('.logged-out').removeClass('show');
+  //     // $('.logged-in').addClass('show');
+  //   }
+  //     });
+  });
+
+  $("#signupButton").click(function(event){
+    event.preventDefault();
+    $("form#signupForm").fadeIn( "slow", function(){
+      });
+  });
+
+  $("#signupAway").click(function(event){
+    event.preventDefault();
+    $("form#signupForm").fadeOut( "slow", function(){
+    });
+  });
+
+  $("form#signupForm").submit(function(event){
+    event.preventDefault();
+  });
+
+
+});
